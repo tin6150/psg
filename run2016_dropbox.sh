56639b054ca2f1c92130c95242e92a38b6b3090c
tin6150 <tin6150@gmail.com>
2016-10-22 15:55:46 -0700

    bulk transform many doc url such as dropbox.com, dropboxusercontent.com, to github.io.  see transform_doc_url.sh for details

---
+++ run2016_dropbox.sh
@@ -0,0 +1,3 @@
+ sed -i "s/unixville.com\/~sn\/psg/tin6150.s3-website-us-west-1.amazonaws.com/g" index.old.20110819.html
+ sed -i "s/unixville.com\/~sn\/psg/tin6150.s3-website-us-west-1.amazonaws.com/g" psg1.html
+ sed -i "s/unixville.com\/~sn\/psg/tin6150.s3-website-us-west-1.amazonaws.com/g" psg-orig.html
