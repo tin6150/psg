97f1721589b6b646ea29ece876a3f50c68fc5c6e
tin <tin6150@gmail.com>
2016-10-24 23:05:18 -0700

    s/ cs.fiu ask-margo.com /g run2016*sh

--- transform_doc_url.sh
+++ transform_doc_url.sh
@@ -1,13 +1,16 @@
 # replace dl.dropboxusercontent.com/u/31360775 to tin6150.github.io 
 # \\\/ to produce / is probably not necessary, at least not in the awk in MINGW64, but it doesn't hurt
  
-fgrep -l "31360775"  *html | awk '{print " sed -i \"s/dl.dropboxusercontent.com\\\/u\\\/31360775/tin6150.github.io/g\" " $0 }'  > run2016_dropbox.sh
+#fgrep -l "31360775"  *html | awk '{print " sed -i \"s/dl.dropboxusercontent.com\\\/u\\\/31360775/tin6150.github.io/g\" " $0 }'  > run2016_dropbox.sh
 
-fgrep -l "31360775"  *html | awk '{print " sed -i \"s/dl.dropbox.com\\\/u\\\/31360775/tin6150.github.io/g\" " $0 }'  > run2016_dropbox.sh
+#fgrep -l "31360775"  *html | awk '{print " sed -i \"s/dl.dropbox.com\\\/u\\\/31360775/tin6150.github.io/g\" " $0 }'  > run2016_dropbox.sh
 
-fgrep -l "dropbox"  *html | awk '{print " sed -i \"s/This new home page at dropbox/This new home page at github/\" " $0 }'  > run2016_dropbox.sh
+#fgrep -l "dropbox"  *html | awk '{print " sed -i \"s/This new home page at dropbox/This new home page at github/\" " $0 }'  > run2016_dropbox.sh
 
 
 # unixville.com/~sn/psg to tin6150.s3-website-us-west-1.amazonaws.com
 # \~ to produce ~ prob not necessary, but don't hurt
-fgrep -l "unixville.com"  *html | awk '{print " sed -i \"s/unixville.com\\\/\~sn\\\/psg/tin6150.s3-website-us-west-1.amazonaws.com/g\" " $0 }'  > run2016_dropbox.sh
+#fgrep -l "unixville.com"  *html | awk '{print " sed -i \"s/unixville.com\\\/\~sn\\\/psg/tin6150.s3-website-us-west-1.amazonaws.com/g\" " $0 }'  > run2016_dropbox.sh
+
+
+fgrep -l "cs.fiu" *html | awk '{print " sed -i \"s/www.cs.fiu.edu\\\/\~tho01\\\/psg/psg.ask-margo.com/g\" " $0 }'  > run2016_dropbox.sh
