0c1a412882a9df3328847213815498eb64462cfc
tin6150 <tin6150@gmail.com>
2017-03-26 15:43:41 -0700

    script to help check TZ name in all timezone

---
+++ script/timezone.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+# testing various ideas in setting TZ
+
+TZ_LIST=$(  find /usr/share/zoneinfo -type f -exec tail -1 {} \; ) 
+for TZ in TZ_LIST; do
+	echo $TZ
+	date -d "TZ=$TZ  2016-02-29 13:45"
+done
+
+
