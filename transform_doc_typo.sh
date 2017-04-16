da3ebb4d80954608e0595aee7abf6fd767be30e7
sn <tin6150@gmail.com>
2017-04-15 22:45:20 -0700

    s/php.html/php.txt/

--- transform_doc_typo.sh
+++ transform_doc_typo.sh
@@ -1,8 +1,12 @@
 # replace toul>.html with tool.html
 # \\\. to produce \. still return warning but works.
-fgrep -l toul\>.html *html | awk '{print " sed -i \"s/toul.\\\.html/tool.html/\" " $0 }' > run1612.sh
+# fgrep -l toul\>.html *html | awk '{print " sed -i \"s/toul.\\\.html/tool.html/\" " $0 }' > generated_run1612.sh
 
 
 # replace "toul review..." with "tool review...", omitting the review word, but account for a space
-fgrep -l toul\>\  *html | awk '{print " sed -i \"s/toul.\\\ /tool\ /\" " $0 }' > run1612.sh
+# fgrep -l toul\>\  *html | awk '{print " sed -i \"s/toul.\\\ /tool\ /\" " $0 }' > generated_run1612.sh
+
+
+# replace php.html with php.txt
+fgrep -l php.html *html | awk '{print " sed -i \"s/php.html/php.txt/\" " $0 }' > generated_fixtypo.sh
 
