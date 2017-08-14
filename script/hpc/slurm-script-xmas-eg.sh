46169d61d8b9ac0ecb411cd63175ac2c1cfb0a95
tin6150 <tin6150@sl7>
2017-08-14 10:28:02 -0700

    improved slurm job script eg

--- script/hpc/slurm-script-xmas-eg.sh
+++ script/hpc/slurm-script-xmas-eg.sh
@@ -2,35 +2,46 @@
 
 # https://sites.google.com/a/lbl.gov/high-performance-computing-services-group/lbnl-supercluster/xmas
 
+# submit a single job as  
+#    sbatch   ./slurm-script-xmas-eg.sh                                          
+
 # to test every node of xmas, run on login node as:
 # cd /global/scratch/tin/tmp/xmas
 # for T in $(seq -w 0000 0019); do
-#         sbatch  --partition=xmas --account=xmas  -w n_${T}.xmas0 --job-name=N${T} /global/home/users/tin/sn-gh/psg/script/hpc/slurm-script-xmas-eg.sh
+#         #sbatch  --partition=xmas --account=xmas  -w n${T}.xmas0 --job-name=N_${T} /global/home/users/tin/sn-gh/psg/script/hpc/slurm-script-xmas-eg.sh
 # done
 
 
+#SBATCH 	--job-name=xmas_test	# CLI arg will overwrite this, no 
+# 		CPU time: 
+#SBATCH 	--time=65
+# 		Wall clock limit:
+# #SBATCH 	--time=00:10:00
+#SBATCH 	--partition=xmas
+# #SBATCH 	-n 4
+# #SBATCH 	--qos=lr_normal
+# #SBATCH 	--qos=lr_lowprio
+#SBATCH 	--account=xmas
+# #SBATCH 	--ntasks=2
+# #SBATCH 	--mail-type=all
+# #SBATCH 	--mail-type=END,FAIL
+#SBATCH 	--mail-type=FAIL
+#SBATCH 	--mail-user=tin@lbl.gov
+# #SBATCH 	-N 3
+#SBATCH 	-o  slurm_out_xmas_%N_job_%j.txt
+# #SBATCH 	-o  junkable_slurm_out_xmas.txt
 
-#SBATCH --job-name=test-xmas
-#SBATCH --time=65
-#SBATCH --partition=xmas
-###SBATCH -n 4
-###SBATCH --qos=lr_normal
-###SBATCH --qos=lr_lowprio
-#SBATCH --account=xmas
-###SBATCH --ntasks=2
-# Wall clock limit:
-##SBATCH --time=00:10:00
-#SBATCH --mail-type=END,FAIL
-##SBATCH --mail-type=all
-#SBATCH --mail-user=tin@lbl.gov
-###SBATCH -N 3
-##SBATCH -o  junkable_slurm_out_xmas.txt
+# %j is for jobid (eg 128)
+# %J is for jobid.stepid (eg 128.0)
+# %N is short node name
+# %n is node number relative to current job, 0 for first node.  task spanning multiple node will get multiple file created
+# -J is --job-name for CLI arg
 
-## multiple node job will write to the same output file, if -o file is specified.  
-## if no -o specified, create output file with JOB ID Number as part of file name in CWD
+## if no -o specified, default to slurm-%j.out or slurm-%A_%s.out for normal and array jobs
+## if not using $j expansion, multiple node job will (over)write to the same output file (not auto concat)
+## -e for stderr, which if not specified, would be combined into stdout
+## https://slurm.schedmd.com/sbatch.html filename pattern : has details of % expansions
 
-# submit job as  
-#    sbatch   ./slurm-script-xmas-eg.sh                                          
 
 
 hostname
