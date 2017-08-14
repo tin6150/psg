9a12520db01b1633c265ced596190c59fd1db04c
tin6150 <tin6150@sl7>
2017-08-14 09:39:13 -0700

    slightly tweaked xmas job script

--- script/hpc/slurm-script-xmas-eg.sh
+++ script/hpc/slurm-script-xmas-eg.sh
@@ -2,9 +2,16 @@
 
 # https://sites.google.com/a/lbl.gov/high-performance-computing-services-group/lbnl-supercluster/xmas
 
+# to test every node of xmas, run on login node as:
+# cd /global/scratch/tin/tmp/xmas
+# for T in $(seq -w 0000 0019); do
+#         sbatch  --partition=xmas --account=xmas  -w n_${T}.xmas0 --job-name=N${T} /global/home/users/tin/sn-gh/psg/script/hpc/slurm-script-xmas-eg.sh
+# done
+
+
 
 #SBATCH --job-name=test-xmas
-#SBATCH --time=5
+#SBATCH --time=65
 #SBATCH --partition=xmas
 ###SBATCH -n 4
 ###SBATCH --qos=lr_normal
@@ -13,19 +20,22 @@
 ###SBATCH --ntasks=2
 # Wall clock limit:
 ##SBATCH --time=00:10:00
-#SBATCH --mail-type=all
+#SBATCH --mail-type=END,FAIL
+##SBATCH --mail-type=all
 #SBATCH --mail-user=tin@lbl.gov
 ###SBATCH -N 3
 ##SBATCH -o  junkable_slurm_out_xmas.txt
 
-## multiple node job will write to the same output file, even when no -o file is specified.  
-## maybe if explicit create task array will create the additional per-task-array-job  output file ?
+## multiple node job will write to the same output file, if -o file is specified.  
+## if no -o specified, create output file with JOB ID Number as part of file name in CWD
 
 # submit job as  
 #    sbatch   ./slurm-script-xmas-eg.sh                                          
 
 
 hostname
+##exit 0		##
+
 uptime
 date
 pwd
@@ -36,9 +46,9 @@ df -hl
 echo ---------------------------------------
 cat /proc/mounts
 echo ---------------------------------------
-#echo "date before sleep"
-#date
-#sleep 180
-#echo "date after sleep"
-#date
+echo "date before sleep"
+date
+sleep 60
+echo "date after sleep"
+date
 
