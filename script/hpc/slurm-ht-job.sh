c60983c27f910495d56a56c0f95b9d681f00c29b
tin6150 <tin@brcmst>
2018-10-22 11:57:36 -0700

    feeling ht_helper.sh  --ntasks/-n = parallel processes

--- script/hpc/slurm-ht-job.sh
+++ script/hpc/slurm-ht-job.sh
@@ -3,15 +3,14 @@
 #SBATCH --partition=savio2_knl
 #SBATCH --qos=normal
 #SBATCH --account=scs
-#SBATCH --ntasks=8          # slurm will create a job with these many cores
-###SBATCH --nodes=3		# detected as 3 slots... avoid use
+#SBATCH --ntasks=130          # slurm will create a job with these many cores
 #SBATCH --mem-per-cpu=2G 
 #SBATCH --time=02:30:00
 
-ht_helper.sh -t ht_taskfile -n1 -s10 -Lv
-# -n1  = number of processors per task
-# -s10 = number of seconds before next check by ht_helper mini fifo scheduler.
-# -L   = log task stdout/stderr to individual files
-# -v   = verbose mode
+ht_helper.sh -t ht_taskfile -n2 -s10 -Lv
+# -n = num of processors per task (--ntasks/-n = num concurrent process)
+# -s = num of sec before next check by ht_helper mini fifo scheduler. def=60
+# -L = log task stdout/stderr to individual files
+# -v = verbose mode
 
 
