7383386ae006e42a5b75a54c616ff32caff40f4d
tin6150 <tin@brcmst>
2020-04-12 22:03:46 -0700

    slurm submit notes after bloody banking plugin restrictions

--- script/hpc/slurm-job.sh
+++ script/hpc/slurm-job.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
-#SBATCH --job-name=test
+
+## #SBATCH --constrain=lr3_normal # these are features in slurm conf  ## slurm bank don't support these on 2020.0312 :(
+## #SBATCH --job-name=test  
+
 #SBATCH --partition=lr3
-#SBATCH --constrain=lr3_normal # these are features in slurm conf
 #SBATCH --qos=lr_normal
 #SBATCH --account=scs
 #SBATCH --nodes=2
 #SBATCH --mem-per-cpu=2G 
 #SBATCH --time=02:30:00
 
+# submit as 
+# sbatch --partition=savio2 -N 1 --account=scs --qos=savio_debug ./slurm-job.sh
 hostname
 date
 uptime
