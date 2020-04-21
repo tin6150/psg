229ce2e28a7d2cce0a8632c51d314240d11aae2a
tin6150 <tin6150@gmail.com>
2020-04-21 13:19:52 -0700

    awk exact text search; bash multi line doc

---
+++ script/sh/comment.bash
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+
+echo "Testing multiline comments in bash"
+
+: '
+	remember fork bomb? 
+	ref: 
+	https://stackoverflow.com/questions/43158140/way-to-create-multiline-comments-in-bash
+	see psg/shellScript.txt for HEREDOC old school method in sh
+'
+
+
+
+"""
+// hmm... 
+// is pythong style triple quote supported now?
+// nope
+// just vim syntax highlight quirk
+"""
+
+echo "The end"
