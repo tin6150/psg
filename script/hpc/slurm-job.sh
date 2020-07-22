48694dad824c4a8c16c5b9115bf18b7c72fc7780
tin6150 <tin@brcmst>
2020-07-22 09:32:28 -0700

    brc nas

--- script/hpc/slurm-job.sh
+++ script/hpc/slurm-job.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 ## #SBATCH --constrain=lr3_normal # these not in brc, so get error
-#SBATCH --constrain=savio2_c28 # other features could be savio2_1080ti, savio2_m128, savio3_m384, savio3_m96, 4rtx, 8rtx  (which should have been 4gtx), savio3_2080ti should just become savio3_gpu?  
+###SBATCH --constrain=savio2_c28 # other features could be savio2_1080ti, savio2_m128, savio3_m384, savio3_m96, 4rtx, 8rtx  (which should have been 4gtx), savio3_2080ti should just become savio3_gpu?  
 ## #SBATCH --job-name=test  
 
-#SBATCH --partition=savio2
-#SBATCH --qos=lr_normal
+#SBATCH --partition=savio
+#SBATCH --qos=savio_normal
 #SBATCH --account=scs
 #SBATCH --nodes=2
 #SBATCH --mem-per-cpu=2G 
