1c485eaf39f3ed5421d5328d1e88816a8b0ea67d
tin6150 <tin+zink@grumpyxmas.com>
2020-08-30 22:22:13 -0700

    zink, mostly R

--- script/R/helloWorld.R
+++ script/R/helloWorld.R
@@ -2,6 +2,19 @@
 
 print("Hello Word from R!")
 
+R.Version()
+
+sI <- sessionInfo()
+sI
+
 library()
 
 
+c <- capabilities()
+c 
+
+
+system('id -a; uname')
+system('env')
+
+# ref: https://stat.ethz.ch/R-manual/R-devel/library/utils/html/sessionInfo.html
