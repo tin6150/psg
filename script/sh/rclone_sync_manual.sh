2421d0b6753771e4ae0abd90ae0fe126c14a05d8
tin6150 <tin@brcmst>
2023-01-21 10:27:32 -0800

    rclone_sync_manual.sh usble with dchiang50gdrv but ran out space quickly

--- script/sh/rclone_sync_manual.sh
+++ script/sh/rclone_sync_manual.sh
@@ -6,9 +6,15 @@
 # brc>
 module load rclone/1.58.1
 
-## rclone ls dchiang50gdrv:/RCLONE_BK_via_SYNC
+##rclone lsd dchiang50gdrv:    # this ran out of space quickly
+##rclone lsd tinberk:          # this was not authorized for dtn.brc, then also failed on bofh... *sigh* need to check...
 
+
+
+## rclone lsd dchiang50gdrv:/RCLONE_BK_via_SYNC
+## rclone lsd dchiang50gdrv:/RCLONE_BK_via_SYNC/global/scratch/users/tin/fc_graham/PRISA/Assemblies/
 ## adapted from z1_rclone_sync.sh
+
 
 ## ++CHANGE_ME++
 LOCAL_BACKUP_LIST="/global/scratch/users/tin/fc_graham/PRISA/Assemblies/animal"
@@ -32,11 +38,12 @@ SUM_EXIT_CODE=0
 
 echo "====================  rclone_sync_manual - $CUR_DATE ============"
 
-run_rclone_push() {
+# noFn wrapper
+#run_rclone_push() {
 	for LOCAL_BACKUP in $LOCAL_BACKUP_LIST; do
 		echo "-------- Processing $LOCAL_BACKUP at $(date) --------"
-echo $RCLONE mkdir $REMOTE_NAME:$ROOT_FOLDER/$LOCAL_BACKUP ## > /dev/null 2>&1
-echo $RCLONE --transfers=$TRANSFER_COUNT --checkers=$CHECKER_COUNT sync $LOCAL_BACKUP  ${REMOTE_NAME}:${ROOT_FOLDER}/${LOCAL_BACKUP} | tee -a $LOGFILE
+		$RCLONE mkdir $REMOTE_NAME:$ROOT_FOLDER/$LOCAL_BACKUP ## > /dev/null 2>&1
+		$RCLONE --transfers=$TRANSFER_COUNT --checkers=$CHECKER_COUNT sync $LOCAL_BACKUP  ${REMOTE_NAME}:${ROOT_FOLDER}/${LOCAL_BACKUP} | tee -a $LOGFILE
 		EXIT_CODE=$?
 		echo "rclone of $LOCAL_BACKUP had exit code of $EXIT_CODE"
 		SUM_EXIT_CODE=$( expr $EXIT_CODE + $SUM_EXIT_CODE )				
@@ -44,9 +51,9 @@ echo $RCLONE --transfers=$TRANSFER_COUNT --checkers=$CHECKER_COUNT sync $LOCAL_B
 	if [[ 0 -ne $SUM_EXIT_CODE  ]]; then
 		echo "SUM_EXIT_CODE is $SUM_EXIT_CODE" | mail -s "WARN: tin@BRC rclone_sync_manual.sh on $HOSTNAME had non zero exit code, please check" "$MAILTO"
 	fi
-}
+#}
 
-run_rclone_push 2>&1 | tee -a $LOGFILE | mail -s "$HOSTNAME - rclone sync daily:mysql_backup - $SUM_EXIT_CODE" "$MAILTO"
+#noFn#run_rclone_push 2>&1 | tee -a $LOGFILE | mail -s "$HOSTNAME - rclone sync manual tin@brc - $SUM_EXIT_CODE" "$MAILTO"
 
 
 
