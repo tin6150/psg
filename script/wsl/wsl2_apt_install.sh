54a6bfa06bd6b45bd76be099ac8e190ff18a8d11
tin6150 <sn+newbox@grumpyxmas.com>
2024-02-29 19:24:37 -0800

    wsl2 LL486

---
+++ script/wsl/wsl2_apt_install.sh
@@ -0,0 +1,22 @@
+
+#### apt stuff
+#### should be mostly idempotent, so can just run on all instance of wsl2 (or other ubuntu-based machine)
+
+
+sudo apt update
+
+sudo apt install  gnome-text-editor -y   # was the recommended thing from learn.microsoft.com
+# gnome-terminal is currently gabbled up cuz bad graphics driver...
+sudo apt install mousepad  # xfce lightweight editor, 3MB
+
+sudo apt install nautilus  -y
+# there is vlc, gimp, google-chrome, microsoft-edge
+
+sudo apt install x11-apps -y   # xcalc , xcloc, xeye.  there are display update error.  may need vGPU driver...
+
+sudo apt install xterm 
+
+
+
+
+
